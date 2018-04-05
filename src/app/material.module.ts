f65eba47b65763cc8a8ff702ecef96a7c44d4708
michael0854 <rmichaelbics@gmail.com>
2018-04-05 20:37:30 +0530

    first commit

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
+// import {MatPaginator, MatTableDataSource} from '@angular/material';
 
 @NgModule ({
-    imports: [MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatRadioModule,
-        MatSelectModule, MatInputModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatRadioModule,
-         MatSelectModule, MatInputModule]
+    imports: [MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatRadioModule, MatSlideToggleModule,
+        MatSelectModule, MatInputModule, MatNativeDateModule, MatSliderModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatRadioModule, MatSlideToggleModule,
+         MatSelectModule, MatInputModule, MatNativeDateModule, MatSliderModule]
 })
 
 export class MaterialModule { }
